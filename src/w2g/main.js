95d524d5033e5c23eb413b158ba920856be35a19
JayKayUk <jacob.kaszczuk@gmail.com>
2022-12-01 18:24:21 +0000

    No longer developed

--- src/w2g/main.js
+++ src/w2g/main.js
@@ -5,7 +5,7 @@ console.log("w2g");
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const cookie = {
     sharedId: document.cookie.match(/_sharedID=.+?;/g).toString(),
-    w2glan: document.cookie.match(/w2glang=.+?;/g).toString(),
+    w2glang: document.cookie.match(/w2glang=.+?;/g).toString(),
     w2gintro: document.cookie.match(/w2g_init=.+?;/g).toString(),
     w2ginit: document.cookie.match(/w2g_intro=.+?;/g).toString(),
   };
@@ -18,5 +18,18 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     "dropdown fluid selection ui"
   )[0].value;
 
-  sendResponse({ roomURL, cookie, playlistURL });
+  fetch(
+    `https://api.w2g.tv/rooms/${roomURL}/playlists/${playlistURL}/playlist_items/sync_update`,
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: `{"add_items":[{"url":"${message.url}","title":"${message.title}","thumb":"${message.thumb}"}]}`,
+    }
+  ).then((fetchResponose) => {
+    if (!fetchResponose.ok) {
+      console.log(fetchResponose);
+    }
+  });
 });
