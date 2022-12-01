d9ddc9acb70cf18daf0b4f80ec5186f577e37ffa
JayKayUk <jacob.kaszczuk@gmail.com>
2022-12-01 16:12:01 +0000

    a: getting info in the w2g script and send to bg

--- src/w2g/main.js
+++ src/w2g/main.js
@@ -3,6 +3,20 @@
 console.log("w2g");
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log(message);
-  sendResponse({ response: "response" });
+  const cookie = {
+    sharedId: document.cookie.match(/_sharedID=.+?;/g).toString(),
+    w2glan: document.cookie.match(/w2glang=.+?;/g).toString(),
+    w2gintro: document.cookie.match(/w2g_init=.+?;/g).toString(),
+    w2ginit: document.cookie.match(/w2g_intro=.+?;/g).toString(),
+  };
+
+  const roomURL = document
+    .getElementById("w2g-top-inviteurl")
+    .children[0].value.split("/")[3];
+
+  const playlistURL = document.getElementsByClassName(
+    "dropdown fluid selection ui"
+  )[0].value;
+
+  sendResponse({ roomURL, cookie, playlistURL });
 });
