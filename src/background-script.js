123fbf98f7ae5e5f6e02ff4ac15940cc0b1efea8
JayKayUk <jacob.kaszczuk@gmail.com>
2022-12-02 11:03:52 +0000

    clean up code, inject script into w2g page

--- src/background-script.js
+++ src/background-script.js
@@ -5,20 +5,7 @@ browser.runtime.onInstalled.addListener(() => {
 });
 
 browser.runtime.onMessage.addListener((message) => {
-  console.log(message);
   browser.tabs.query({ url: "https://w2g.tv/*/room/*" }).then((tabs) => {
-    console.log(tabs[0].id);
-    browser.tabs.sendMessage(tabs[0].id, { message });
+    browser.tabs.sendMessage(tabs[0].id, message);
   });
 });
-
-// fetch(
-//   "https://api.w2g.tv/rooms/gcmxwjue1f9feg1zru/playlists/0b8ast7zmgrr91y33ue2cfew16rpjg0u/playlist_items/sync_update",
-//   {
-//     method: "POST",
-//     headers: {
-//       "content-type": "application/json",
-//     },
-//     body: '{"add_items":[{"url":"//www.youtube.com/watch?v=27P-3hWwqSo","title":"Er war die erste Nummer 1 | Smitty Werben Jagger Man Jensen","thumb":"https://i.ytimg.com/vi/27P-3hWwqSo/mqdefault.jpg"}]}',
-//   }
-// );
--- src/w2g/main.js
+++ src/w2g/main.js
@@ -1,15 +1,6 @@
 "use strict";
 
-console.log("w2g");
-
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  const cookie = {
-    sharedId: document.cookie.match(/_sharedID=.+?;/g).toString(),
-    w2glang: document.cookie.match(/w2glang=.+?;/g).toString(),
-    w2gintro: document.cookie.match(/w2g_init=.+?;/g).toString(),
-    w2ginit: document.cookie.match(/w2g_intro=.+?;/g).toString(),
-  };
-
   const roomURL = document
     .getElementById("w2g-top-inviteurl")
     .children[0].value.split("/")[3];
@@ -18,18 +9,24 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     "dropdown fluid selection ui"
   )[0].value;
 
-  fetch(
-    `https://api.w2g.tv/rooms/${roomURL}/playlists/${playlistURL}/playlist_items/sync_update`,
+  const code = `fetch(
+    "https://api.w2g.tv/rooms/${roomURL}/playlists/${playlistURL}/playlist_items/sync_update",
     {
+      credentials: "include",
       method: "POST",
       headers: {
         "content-type": "application/json",
       },
-      body: `{"add_items":[{"url":"${message.url}","title":"${message.title}","thumb":"${message.thumb}"}]}`,
+      body: '{"add_items":[{"url":"${message.url}","title":"${message.title}","thumb":"${message.thumb}"}]}',
     }
   ).then((fetchResponose) => {
     if (!fetchResponose.ok) {
       console.log(fetchResponose);
     }
-  });
+  });`;
+
+  const script = document.createElement("script");
+  script.textContent = code;
+  (document.head || document.documentElement).appendChild(script);
+  script.remove();
 });
