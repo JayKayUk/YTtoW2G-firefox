81303695365be3301325d31ba9906578a42b2017
JayKay <kubus729@gmail.com>
2022-12-13 13:06:15 +0000

    Refactor yt/main w2g/main

--- src/yt/main.js
+++ src/yt/main.js
@@ -1,81 +1,59 @@
 "use strict";
 
-function stringSanitize(text) {
+function sanitizeString(text) {
   return text.replace(/['"`/\\]/g, "");
 }
 
-function addListeners() {
-  // Run until arrayOfVideosTiles is filled in a grid mode old UI
-  let arrayOfVideosTiles = [];
-  let numberOfVideoPreAdding = 0;
-  const interval = setInterval(() => {
-    arrayOfVideosTiles = document.getElementsByTagName(
-      "ytd-grid-video-renderer"
-    );
-    const buttonHTMLstring = `<button id='YTtoW2G'><img src='${browser.runtime.getURL(
-      "icons/main-icon-2048.png"
-    )}'></button>`;
-    console.log(arrayOfVideosTiles.length, numberOfVideoPreAdding);
-    if (
-      numberOfVideoPreAdding % 100 === 0 &&
-      arrayOfVideosTiles.length !== 0 &&
-      arrayOfVideosTiles.length % 100 === 0
-    ) {
-      numberOfVideoPreAdding = arrayOfVideosTiles.length;
-      clearInterval(interval);
+function addButton(e) {
+  const videoTile = e.currentTarget;
 
-      // add listeners for every video in a grid
-      for (let i = 0; i < arrayOfVideosTiles.length; i++) {
-        // add button at the end of ytd-grid-video-render node on mouse enter. Need to getElementById because can't add listener to element that does not exist
-        arrayOfVideosTiles[i].addEventListener("mouseenter", (e) => {
-          const videoTile = e.currentTarget;
-          if (!videoTile.querySelector("#YTtoW2G")) {
-            videoTile.insertAdjacentHTML("beforeend", buttonHTMLstring);
-            const button = document.getElementById("YTtoW2G");
-            button.addEventListener("click", () => {
-              const videoURL = videoTile.querySelector("#thumbnail").href;
-              const thumbnailSource = videoTile.querySelector("img").src;
-              const videoTitle = stringSanitize(
-                videoTile.querySelector("#video-title").title
-              );
-              console.log(videoTitle);
-              browser.runtime.sendMessage({
-                url: videoURL,
-                thumb: thumbnailSource,
-                title: videoTitle,
-              });
-            });
-          }
-        });
+  // Check if button already exists
+  if (videoTile.querySelector("#YTtoW2G")) {
+    return;
+  }
 
-        // remove button when cursour leaves ytd-grid-video-render node. No need to remove listener as the engine just ignores addEventListener if a listener exists already
-        arrayOfVideosTiles[i].addEventListener("mouseleave", (e) => {
-          const buttonToRemove = document.getElementById("YTtoW2G");
-          e.currentTarget.removeChild(buttonToRemove);
-        });
-      }
-    }
+  const buttonHTMLstring = `<button id='YTtoW2G'><img src='${browser.runtime.getURL(
+    "icons/main-icon-2048.png"
+  )}'></button>`;
+  videoTile.insertAdjacentHTML("beforeend", buttonHTMLstring);
 
-    // console.log("YTtoW2G - Number of videos:" + arrayOfVideosTiles.length);
-  }, 100);
+  addButtonListener(videoTile);
 }
 
-addListeners();
+function addButtonListener(videoTile) {
+  const button = document.getElementById("YTtoW2G");
+  button.addEventListener("click", () => {
+    sendMsgToBgScript(videoTile);
+  });
+}
 
-document.addEventListener("wheel", () => {
-  if (window.scrollY > window.scrollMaxY * 0.9) {
-    const observerTarget = document;
-    const observerOptions = { childList: true, subtree: true };
-    const observer = new MutationObserver((mutationList) => {
-      for (let i = 0; i < mutationList.length; i++) {
-        if (mutationList[i].target.tagName === "YTD-ITEM-SECTION-RENDERER") {
-          console.log(mutationList[i].target);
-          addListeners();
-          observer.disconnect();
-          break;
-        }
-      }
-    });
-    observer.observe(observerTarget, observerOptions);
+function sendMsgToBgScript(videoTile) {
+  const videoURL = videoTile.querySelector("#thumbnail").href;
+  const thumbnailSource = videoTile.querySelector("img").src;
+  const videoTitle = sanitizeString(
+    videoTile.querySelector("#video-title").title
+  );
+  browser.runtime.sendMessage({
+    url: videoURL,
+    thumb: thumbnailSource,
+    title: videoTitle,
+  });
+}
+
+function removeButton(e) {
+  const buttonToRemove = document.getElementById("YTtoW2G");
+  e.currentTarget.removeChild(buttonToRemove);
+}
+
+const observerOptions = { childList: true, subtree: true };
+const observerTarget = document;
+const observer = new MutationObserver((mutationList) => {
+  for (let i = 0; i < mutationList.length; i++) {
+    if (mutationList[i].target.tagName !== "YTD-GRID-VIDEO-RENDERER") {
+      continue;
+    }
+    mutationList[i].target.addEventListener("mouseenter", addButton);
+    mutationList[i].target.addEventListener("mouseleave", removeButton);
   }
 });
+observer.observe(observerTarget, observerOptions);
