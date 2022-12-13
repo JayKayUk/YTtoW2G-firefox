698e1b940daf75fe2b6ad34830e11ce9c82a6f8f
JayKayUk <jacob.kaszczuk@gmail.com>
2022-12-13 13:00:12 +0000

    ref: w2g/main

--- src/w2g/main.js
+++ src/w2g/main.js
@@ -1,6 +1,13 @@
 "use strict";
 
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+function runScriptElement(code) {
+  const script = document.createElement("script");
+  script.textContent = code;
+  (document.head || document.documentElement).appendChild(script);
+  script.remove();
+}
+
+function getScriptCode(message) {
   const roomURL = document
     .getElementById("w2g-top-inviteurl")
     .children[0].value.split("/")[3];
@@ -9,7 +16,7 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     "dropdown fluid selection ui"
   )[0].value;
 
-  const code = `fetch(
+  return `fetch(
     "https://api.w2g.tv/rooms/${roomURL}/playlists/${playlistURL}/playlist_items/sync_update",
     {
       credentials: "include",
@@ -24,9 +31,11 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
       console.log(fetchResponose);
     }
   });`;
+}
 
-  const script = document.createElement("script");
-  script.textContent = code;
-  (document.head || document.documentElement).appendChild(script);
-  script.remove();
-});
+function addVideoToPlaylist(message) {
+  const code = getScriptCode(message);
+  runScriptElement(code);
+}
+
+browser.runtime.onMessage.addListener(addVideoToPlaylist);
